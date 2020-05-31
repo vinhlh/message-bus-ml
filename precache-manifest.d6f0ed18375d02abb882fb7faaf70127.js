dc8def522149fac7d94d06177e5497cdb2d7a576
Vinh Le <vinh.rocks@icloud.com>
2020-06-01 00:03:12 +0800

    Updates

---
+++ precache-manifest.d6f0ed18375d02abb882fb7faaf70127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad898cf473759e101391c3cc20bc7e76",
+    "url": "/message-bus-ml/index.html"
+  },
+  {
+    "revision": "f5e30577bb2d47a34ade",
+    "url": "/message-bus-ml/static/js/2.22250701.chunk.js"
+  },
+  {
+    "revision": "139e16100b8d04c07360f902f8d1d6f3",
+    "url": "/message-bus-ml/static/js/2.22250701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bbfce96990e139c65c",
+    "url": "/message-bus-ml/static/js/main.d4293bc7.chunk.js"
+  },
+  {
+    "revision": "83525638d20a61f1cb6f",
+    "url": "/message-bus-ml/static/js/runtime-main.1cc01777.js"
+  }
+]);
